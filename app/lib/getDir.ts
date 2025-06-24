6bf028ce8c527ef97c8e591ebdd7140ff6f24787
r-4bb1t <r4bb1t.github@gmail.com>
2025-06-25 00:07:52 +0900

    ✨ Feat: Implement getDir function to retrieve directory and generate children if none exist

---
+++ app/lib/getDir.ts
@@ -0,0 +1,37 @@
+import sql from "better-sqlite3";
+import { fakerKO as faker } from "@faker-js/faker";
+
+const db = sql("dir.db");
+
+export const getDir = async (id: ID): Promise<Dir> => {
+  const dir = db.prepare("SELECT * FROM directory WHERE id = ?").get(id) as DirModel | undefined; // 데이터 가져오기
+  if (!dir) {
+    throw new Error("Directory not found");
+  }
+
+  let children = db.prepare("SELECT * FROM directory WHERE parent = ?").all(id) as DirModel[];
+  if (children.length === 0) {
+    for (let i = 0; i < 10; i++) {
+      const randomName = faker.internet.emoji() + " " + faker.word.noun();
+
+      const result = db
+        .prepare("INSERT INTO directory (name, parent, ancestors) VALUES (?, ?, ?)")
+        .run(randomName, id, JSON.stringify([...JSON.parse(dir.ancestors), { id: dir.id, name: dir.name }]));
+
+      children.push({
+        id: result.lastInsertRowid,
+        name: randomName,
+        parent: id,
+        ancestors: JSON.stringify([...JSON.parse(dir.ancestors), { id: dir.id, name: dir.name }]),
+      });
+    }
+  }
+
+  return {
+    id: dir.id,
+    name: dir.name,
+    parent: dir.parent,
+    ancestors: JSON.parse(dir.ancestors),
+    children,
+  };
+};
