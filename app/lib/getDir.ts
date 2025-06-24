4a04d2031a6bb2f4379e7a72b529a8b240a072d7
r-4bb1t <r4bb1t.github@gmail.com>
2025-06-25 00:54:18 +0900

    🗃️ Refactor: Remove initdb.ts and integrate database initialization into getDir function

--- app/lib/getDir.ts
+++ app/lib/getDir.ts
@@ -1,20 +1,46 @@
 import sql from "better-sqlite3";
 import { fakerKO as faker } from "@faker-js/faker";
 
-const db = sql("dir.db");
+let db: sql.Database | null = null;
+
+const getDatabase = () => {
+  if (!db) {
+    db = sql(":memory:");
+
+    db.prepare(
+      `
+       CREATE TABLE IF NOT EXISTS directory (
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           name TEXT NOT NULL,
+           parent INTEGER,
+           ancestors TEXT NOT NULL
+        )
+    `
+    ).run();
+
+    db.prepare("INSERT INTO directory (name, parent, ancestors) VALUES (?, ?, ?)").run(
+      "root",
+      null,
+      JSON.stringify([])
+    );
+  }
+  return db;
+};
 
 export const getDir = async (id: ID): Promise<Dir> => {
-  const dir = db.prepare("SELECT * FROM directory WHERE id = ?").get(id) as DirModel | undefined; // 데이터 가져오기
+  const database = getDatabase();
+
+  const dir = database.prepare("SELECT * FROM directory WHERE id = ?").get(id) as DirModel | undefined; // 데이터 가져오기
   if (!dir) {
     throw new Error("Directory not found");
   }
 
-  let children = db.prepare("SELECT * FROM directory WHERE parent = ?").all(id) as DirModel[];
+  let children = database.prepare("SELECT * FROM directory WHERE parent = ?").all(id) as DirModel[];
   if (children.length === 0) {
     for (let i = 0; i < 10; i++) {
       const randomName = faker.internet.emoji() + " " + faker.word.noun();
 
-      const result = db
+      const result = database
         .prepare("INSERT INTO directory (name, parent, ancestors) VALUES (?, ?, ?)")
         .run(randomName, id, JSON.stringify([...JSON.parse(dir.ancestors), { id: dir.id, name: dir.name }]));
 
